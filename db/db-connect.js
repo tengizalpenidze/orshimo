0877c36898dca5d82caaf48fa178d19b0aea820d
tengizalpenidze <tengizalpenidze@gmail.com>
2022-05-26 23:15:01 +0200

    Removed all files that are in the .gitignore

--- db/db-connect.js
+++ db/db-connect.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 
-const sequelize = new Sequelize('orshimo', 'tengo', 'Aa123456', {
+const sequelize = new Sequelize('cooking_app', 'sqluser', 'Aa123456', {
   host: '127.0.1',
   dialect: 'mysql'
 });
