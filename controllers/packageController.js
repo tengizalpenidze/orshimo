b33d44fa8959bce0be769c659afba3d816c607dd
tengizalpenidze <tengizalpenidze@gmail.com>
2022-06-01 10:44:15 +0200

    translation of packages added

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -1,7 +1,8 @@
 var Package = require('../models/package');
-
 const fs = require('fs');
 const path = require('path');
+const e = require('express');
+const { LanguageDetector } = require('i18next-http-middleware');
 
 
 
@@ -12,7 +13,6 @@ exports.package_add_get = async function(req, res) {
 
 exports.get_package_details = async function(req, res) {
     let package = await Package.findByPk(parseInt(req.params.packageId));
-    console.log(req.params.packageId);
 
     res.render('package_details', {package: package});
 };
@@ -36,10 +36,14 @@ exports.package_delete_post = async function(req, res) {
     console.log(req.body.packageId);
     packageToDelete = await Package.findByPk(parseInt(req.body.packageId));
     const pathToImage = packageToDelete.packageCoverPhoto;
+    const pathToImage2 = packageToDelete.packageCoverPhoto2 ? path.join(process.cwd(), '/public/'+packageToDelete.packageCoverPhoto2) : null;
+    const pathToImage3 = packageToDelete.packageCoverPhoto3 ? path.join(process.cwd(), '/public/'+packageToDelete.packageCoverPhoto3) : null;
     try{
         await packageToDelete.destroy();
         let realPathToImage = path.join(process.cwd(), '/public/'+pathToImage);
         fs.unlinkSync(realPathToImage);
+        if(pathToImage2) fs.unlinkSync(pathToImage2);
+        if(pathToImage3) fs.unlinkSync(pathToImage3);
     }catch(err){
         console.error(err);
     }
@@ -48,8 +52,6 @@ exports.package_delete_post = async function(req, res) {
 
 exports.package_add_post = async function(req, res) {
     let package = Package.build(req.body);
-
-
 
     for (let i=0; i<req.files.length; i++){
         const newPath = `public/images/uploads/`+ Date.now() + req.files[i].originalname;
@@ -67,7 +69,8 @@ exports.package_add_post = async function(req, res) {
        });
     }
 
-    await package.save();
+    package.save();
+ 
     res.redirect('/');
     
 };
